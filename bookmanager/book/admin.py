6fb2c8f3c885adc8cc7e1b964bd1dde7a0e0c77f
wei123-demo <1951425751@qq.com>
2025-09-05 22:31:09 +0800

    admin站点

--- bookmanager/book/admin.py
+++ bookmanager/book/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from book.models import BookInFo,PropleInFo
+
+admin.site.register(BookInFo)
+admin.site.register(PropleInFo)
--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+#自动生成主键 --id
+class BookInFo(models.Model):
+
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+class PropleInFo(models.Model):
+    name = models.CharField(max_length=10)
+    gender = models.BooleanField()
+
+    def __str__(self):
+        return self.name, self.gender
+
+    book = models.ForeignKey(BookInFo, on_delete=models.CASCADE)
