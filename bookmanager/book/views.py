23fb9f836382a156eb314ebe4c459ab4e1db9dea
wei123-demo <1951425751@qq.com>
2025-09-06 09:49:22 +0800

    子应用的urls

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+#视图函数
+#请求（request）from django.http import HttpRequest
+#返回HttpResponse   from django.http import HttpResponse
+
+def index(request):
+
+    return HttpResponse("Hello, Django!")
--- bookmanager/bookmanager/urls.py
+++ bookmanager/bookmanager/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.urls import include
+from book.views import index
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('book.urls')),
+
+
+
+
 ]
